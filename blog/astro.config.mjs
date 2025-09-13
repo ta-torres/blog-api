a9672f3bb4e3c5d84c8e46af5cda8b876d0d1d9e
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-09-13 19:56:14 -0300

    fix

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,8 +6,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), react()],
+  adapter: vercel(),
 });
